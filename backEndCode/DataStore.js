ad666511b81dea13a3eebf3b74e611aaa7048dac
Justin Patterson <jus.patterson@gmail.com>
2022-09-07 15:42:44 +1000

    set up jquery and back end code files

---
+++ backEndCode/DataStore.js
@@ -0,0 +1,5 @@
+
+
+function test() {
+    $('#test').text($('#test').text() + " test");
+}
---
+++ backEndCode/LoginToTheSystem.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    test1();
+
+})
+
+function test1() {
+    $('#test').text( $('#test').text() + " test 1");
+    test();
+
+
+}
