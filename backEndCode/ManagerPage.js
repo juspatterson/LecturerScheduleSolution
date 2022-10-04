70cdede4f61201c561ab159aef53ce5955673ab4
Justin Patterson <jus.patterson@gmail.com>
2022-10-04 12:50:55 +1100

    added selecting tables rows

--- backEndCode/ManagerPage.js
+++ backEndCode/ManagerPage.js
@@ -1,13 +1,18 @@
 $(function () {
     loadInstanceTable()
     loadLecturesTable()
-})
 
+
+    $('#instances-table, #lectures-table').on('click', 'tr', function () {
+        filterOnSelectedRow()
+    })
+})
 
 function loadInstanceTable() {
     $.ajax({
 
         type: "GET",
+        //async: false,
         url: '../MockUpData/SubjectsTimeTable.json',
         dataType: 'json',
         data: '{}',
@@ -26,16 +31,16 @@ function loadInstanceTable() {
                 responsive: true,
                 paging: false,
                 fixedHeader: true,
-
+                select: true,
                 columns: [
                     {'data': 'SubjectCode'},
                     {'data': 'SubjectName'},
                     {'data': 'StartMonth'},
                     {'data': 'EndMonth'},
                     {'data': 'NumberOfStudents'}
-                ]
+                ],
+                stateSave: true,
             });
-
 
         },
         error: function (obj, textStatus) {
@@ -52,7 +57,7 @@ function loadLecturesTable() {
         dataType: 'json',
         data: '{}',
         success: function (lectureInformation, textStatus) {
-            $('#lectures-table').DataTable({
+            var table = $('#lectures-table').DataTable({
                 autoWidth: true,
                 sScrollY: '80vh',
                 data: lectureInformation.lectures,
@@ -62,12 +67,18 @@ function loadLecturesTable() {
                 responsive: true,
                 paging: false,
                 fixedHeader: true,
+                select: true,
 
                 columns: [
                     {'data': 'name'},
-                    {'data': 'load'}
+                    {'data': 'load'},
+                    {
+                        'data': "subjectsLecturerCanTeach.subjectsCode",
+                        "visible": false
+                    }
                 ]
             });
+
 
 
         },
@@ -75,4 +86,42 @@ function loadLecturesTable() {
             alert(obj.msg);
         }
     });
+}
+
+function filterOnSelectedRow() {
+    var tables = $('.dataTable').DataTable();
+    var instancesTable = tables.table( 0 );
+    var lectureTable = tables.table( 1 );
+
+    selectedRowFromInstancesTable()
+    selectedRowFromLecturersTable()
+
+    var instancesSelectedData =  $('#instances-selected-data').data()
+    var lecturersSelectedData =  $('#lecturers-selected-data').data()
+
+    if (instancesSelectedData != 0){
+        console.log('dongs')
+    }
+    console.log("instancesSelectedData")
+    console.log(instancesSelectedData)
+
+    console.log("lecturersSelectedData")
+    console.log(lecturersSelectedData)
+
+
+    function selectedRowFromInstancesTable() {
+        instancesTable
+            .on('select', function (e, dt, type, indexes) {
+                var rowData = instancesTable.rows({ selected: true}).data();
+                $('#instances-selected-data').data(rowData)
+            });
+    }
+    function selectedRowFromLecturersTable() {
+        lectureTable
+            .on('select', function (e, dt, type, indexes) {
+                var lectureTable1 = lectureTable.rows({ selected: true}).data();
+                $('#lecturers-selected-data').data(lectureTable1)
+            });
+    }
+
 }
