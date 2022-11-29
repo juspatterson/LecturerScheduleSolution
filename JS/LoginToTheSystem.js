4c910d3673cd0467987cdb8816a3b4207d07c598
Justin Patterson <jus.patterson@gmail.com>
2022-11-29 12:37:27 +1100

    add ajax call to sever for logging in

--- JS/LoginToTheSystem.js
+++ JS/LoginToTheSystem.js
@@ -4,9 +4,28 @@ $(document).ready(function () {
 
 function login() {
     $('#login-button').on('click', function (event) {
-        event.preventDefault();
-        alert("Username or Password are incorrect.")
+        $.ajax({
+            type: 'POST',
+            // dataType: 'jsonp',
+            url: '/api/login',
+            async: true,
+            data: {'username': $('#username').val(),'password':  $('#password').val() },
+            success: function (position) {
+                var ojb = JSON.parse(position)
+
+                if (ojb == null) {
+                    alert("Username or Password are incorrect.")
+                }
+                else if (ojb.position == "manager") {
+                    window.location.assign('https://stick-dream.bnr.la/manager.html');
+                }
+                else if (ojb.position == "academic") {
+                    window.location.assign('https://stick-dream.bnr.la/academic.html');
+                }
+            },
+            error: function (obj, textStatus) {
+                console.log(obj.msg);
+            }
+        });
     })
-
-
 }
